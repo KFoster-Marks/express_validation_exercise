8b7f8c8b79e7748d56c8af4565273b9cb5447bc6
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-06-27 16:10:18 -0700

    first commit

--- routes/signup.js
+++ routes/signup.js
@@ -3,36 +3,67 @@
 var express = require('express');
 var router = express.Router();
 
-// Write your form and validations inside this route file.
-
-
-// Add form validations in /signup for:
-// Username: Required. Must be more than 6 characters, must start with a letter, and no punctuation.
-// Password: Required. Must be more than 8 characters with atleast One letter, one number, and one special character (!?/.,')
-// Email: Required. Must be formatted like an email, (something @ something . something)
-// First Name: Required.
-// Last Name: Required.
-// Phone Number: Required. Must be a 10 digit number formatted like: 999-888-9898
-//
-// STRETCH: Hook up a database that you insert these values into after you've validated them.
-// REMINDER: Don't store passwords in plain text.. Make sure you hash it first!
 
 router.get('/', function(req, res){
-  // Handle initial rendering here.
-  res.render('signup', {});
+  res.render('signup', {
+    hasError : false,
+    username: '',
+    password: '',
+    email: '',
+    firstname: '',
+    lastname: '',
+    phone: '',
+  });
 });
 
 router.post('/', function(req, res){
-  // Handle rendering / redirecting here.
+  console.log(req.body);
+  var info = {
+    hasError: false
+  };
+  info.error = {};
+  checkRequired(info, req);
+  checkUsername(req, info);
 
-  // If there arent any validation errors, redirect to '/'
-
-  // If there are validation errors, re-render the signup page, injecting the users previous inputs.
-  res.render('signup', {});
-
+  res.render('signup', info);
 });
 
+function checkUsername(req, info) {
+  info.username = req.body.username;
+  info.error.username = [];
+  //if username less than equal to six
+  if(req.body.username.length <= 6) {
+    info.hasError = true;
+    info.error.username.push({message: "Username is too short"});
+  }
+  if(req.body.username) {
+    var regex = /[A-Za-z]/;
+    if (!req.body.username[0].match(regex)) {
+      info.hasError = true;
+      info.error.username.push({message: "Username should start with a letter"});
+    }
+    var regex = /\W/g;
+    if (req.body.username[0].match(regex)) {
+      info.hasError = true;
+      info.error.username.push({message: "Username should start with a letter"});
+    }
+  }
+}
 
+function checkRequired(info, req) {
+  for(var item in req.body){
+    info[item] = req.body[item];
+    if(req.body[item].length <= 0)
+    {
+      if(!info.error[item])
+      {
+        info.error[item] = [];
+      }
+      info.hasError = true;
+      info.error[item].push({message: item + " is required."});
+    }
+  }
+}
 // PRO-TIP: Write ALOT of functions to help you handle each little piece.
 
 
