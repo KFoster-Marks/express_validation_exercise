a0102f743b3d8408a72a099241884f32e5c909f0
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-06-27 19:07:31 -0700

    finished email val

--- routes/signup.js
+++ routes/signup.js
@@ -24,38 +24,23 @@ router.post('/', function(req, res){
   info.error = {};
   checkRequired(info, req);
   checkUsername(req, info);
-
-  res.render('signup', info);
+  checkPassword(req, info);
+  checkEmail(info, req);
+  if (info.hasError === true) {
+    res.render('signup', info);
+  } else {
+    res.redirect('/');
+  }
 });
 
-function checkUsername(req, info) {
-  info.username = req.body.username;
-  info.error.username = [];
-  //if username less than equal to six
-  if(req.body.username.length <= 6) {
-    info.hasError = true;
-    info.error.username.push({message: "Username is too short"});
-  }
-  if(req.body.username) {
-    var regex = /[A-Za-z]/;
-    if (!req.body.username[0].match(regex)) {
-      info.hasError = true;
-      info.error.username.push({message: "Username should start with a letter"});
-    }
-    var regex = /\W/g;
-    if (req.body.username[0].match(regex)) {
-      info.hasError = true;
-      info.error.username.push({message: "Username should start with a letter"});
-    }
-  }
-}
-
 function checkRequired(info, req) {
-  for(var item in req.body){
+  console.log("Entering checkRequired function");
+  for( var item in req.body ) {
     info[item] = req.body[item];
-    if(req.body[item].length <= 0)
+    if( req.body[item] === '' )
     {
-      if(!info.error[item])
+      console.log("Recognized that nothing is there");
+      if( !info.error[item] )
       {
         info.error[item] = [];
       }
@@ -64,7 +49,73 @@ function checkRequired(info, req) {
     }
   }
 }
-// PRO-TIP: Write ALOT of functions to help you handle each little piece.
+
+function checkUsername(req, info) {
+  info.username = req.body.username;
+  info.error.username = [];
+  //if username less than equal to six
+  if(req.body.username.length <= 6) {
+    info.hasError = true;
+    info.error.username.push({message: "Username is too short."});
+  }
+  if(req.body.username) {
+    var regex = /[A-Za-z]/;
+    if (!req.body.username[0].match(regex)) {
+      info.hasError = true;
+      info.error.username.push({message: "Username should start with a letter."});
+    }
+    var regex = /\W/g;
+    if (req.body.username.match(regex)) {
+      info.hasError = true;
+      info.error.username.push({message: "Username can't contain punctation."});
+    }
+  }
+}
+
+function checkPassword(req, info) {
+  info.password = req.body.password;
+  info.error.password = [];
+  if(req.body.password.length <= 8) {
+    info.hasError = true;
+    info.error.password.push({message: "Password should be 9 or more characters."});
+  }
+  var regex = /[A-Za-z]/;
+  if (!req.body.password.match(regex)) {
+    info.hasError = true;
+    info.error.password.push({message: "Password must contain at least one letter."});
+  }
+  var regex = /\W/g;
+  if (!req.body.password.match(regex)) {
+    info.hasError = true;
+    info.error.password.push({message: "Password must contain at least one special character."});
+  }
+}
+
+function checkEmail(info, req) {
+  var emailString = req.body.email;
+  var atFound = false;
+  var dotFound = false;
+
+  for(var i=1; i < emailString.length; i++) {
+    if(emailString[i] === '@' || atFound)  {
+      if(atFound && emailString[i] === '.')  {
+        //This email has an @ and dot in the right order.
+        dotFound = true;
+      }
+      atFound = true;
+    }
+  }
+
+  if (atFound && dotFound) {
+    info.email = req.body.email;
+  } else {
+    if(!info.error.email) {
+      info.error.email = [];
+    }
+    info.hasError = true;
+    info.error.email.push({message : "email is malformed."});
+  }
+}
 
 
 module.exports = router;
